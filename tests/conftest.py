ca6e85b84ca8f69776cc26108da7e5a5dd63a4be
Mery-debug <m.a.shap58000@gmail.com>
2024-11-10 15:58:25 +0300

    !@#$% README

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,24 @@
 import pytest
+
+from src.Class_Category import Category
 from src.Class_Product import Product
 
 
 @pytest.fixture()
-def product_apple():
-    return Product('apple', 'sweet', 129, 15)
+def product_apple() -> Product:
+    return Product("apple", "sweet", 129, 15)
 
+
+@pytest.fixture()
+def category_food() -> Category:
+    return Category("food", "some food", ["apple", "banana", "meat", "milk", "eggs"])
+
+
+@pytest.fixture()
+def lst_products() -> list:
+    return ["apple", "banana", "meat", "milk", "eggs"]
+
+
+@pytest.fixture()
+def category_magazines() -> Category:
+    return Category("magazines", "sth to read", ["magazines", "newspapers", "book"])
