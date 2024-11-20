dad6b8850d115e332101eb85bba0bd0935b03930
Mery-debug <m.a.shap58000@gmail.com>
2024-11-20 15:44:05 +0300

    test category 81%

--- tests/test_Class_Category.py
+++ tests/test_Class_Category.py
@@ -47,14 +47,13 @@ class TestCase(unittest.TestCase):
 
 
 def test_add_category() -> None:
-    Category.product_count = 0
     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
                              "S23 Ultra", 256, "Серый")
     smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
     smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
     category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
     category_smartphones.add_product(smartphone3)
-    assert Category.product_count == 3
+    assert category_smartphones.product_count == 3
 
 
 
