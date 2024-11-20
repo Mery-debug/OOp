d5065c95572c96e8ca889eb7be9a7338d9d94854
Mery-debug <m.a.shap58000@gmail.com>
2024-11-20 14:58:55 +0300

    class in test!!!

--- tests/test_Class_Category.py
+++ tests/test_Class_Category.py
@@ -1,5 +1,7 @@
+import unittest
+
 from src.Class_Category import Category
-from src.Class_Product import Product
+from src.Class_Product import Product, Smartphone
 
 
 def test_category_init(category_food: Category, lst_products: list) -> None:
@@ -31,3 +33,30 @@ def test_category(str_exp) -> None:
         [product1, product2, product3],
     )
     assert category1.products == str_exp
+
+
+class TestCase(unittest.TestCase):
+    """ Тест класс для обработки выбрасываемой ошибки typeerror """
+    def test_add_product(self):
+        smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                                 "S23 Ultra", 256, "Серый")
+        smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+        category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+        with self.assertRaises(TypeError):
+            category_smartphones.add_product("Not a product")
+
+
+def test_add_category() -> None:
+    Category.product_count = 0
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                             "S23 Ultra", 256, "Серый")
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+    category_smartphones.add_product(smartphone3)
+    assert Category.product_count == 3
+
+
+
+
+
