6e206cf2b3823e889805e63c1991890c2e46eaa7
Mery-debug <m.a.shap58000@gmail.com>
2024-11-20 13:56:24 +0300

    new classes

--- src/Class_Product.py
+++ src/Class_Product.py
@@ -29,8 +29,28 @@ class Product:
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт'
 
     def __add__(self, other):
-        add = self.__price * self.quantity + other.price * other.quantity
-        return add
+        if isinstance(type(self), type(other)):
+            add = self.__price * self.quantity + other.price * other.quantity
+            return add
+
+        raise TypeError
 
 
+class Smartphone(Product):
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency: str = efficiency
+        self.model: str = model
+        self.memory: str = memory
+        self.color: str = color
+
+
+class LawnGrass(Product):
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country: str = country
+        self.germination_period: str = germination_period
+        self.color: str = color
 
